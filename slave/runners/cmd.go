241849fbd421b5f73097024f6732a51b57a02137
Ondrej Kupka <ondra.cap@gmail.com>
2014-05-08 22:40:31 +0200

    Add batch script runner (cmd.exe)

--- slave/runners/cmd.go
+++ slave/runners/cmd.go
@@ -17,32 +17,20 @@
 
 package runners
 
-import "os/exec"
+import (
+	"os/exec"
+	"path/filepath"
+)
 
-type Runner struct {
-	Name       string
-	NewCommand func(script string) *exec.Cmd
-}
-
-var factories = [...]func() *Runner{
-	bashFactory,
-	cmdFactory,
-	powerShellFactory,
-	nodeFactory,
-}
-
-var Available = make([]*Runner, 0)
-
-func init() {
-	ch := make(chan *Runner, len(factories))
-	for i := range factories {
-		go func(index int) {
-			ch <- factories[index]()
-		}(i)
+func cmdFactory() *Runner {
+	if exec.Command("cmd.exe", "/c", "echo foobar").Run() != nil {
+		return nil
 	}
-	for _ = range factories {
-		if runner := <-ch; runner != nil {
-			Available = append(Available, runner)
-		}
+
+	return &Runner{
+		Name: "cmd",
+		NewCommand: func(script string) *exec.Cmd {
+			return exec.Command("cmd.exe", "/c", filepath.FromSlash(script))
+		},
 	}
 }
