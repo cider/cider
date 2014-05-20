dd62c5244092da229d4136fca3a3439b5bc0751b
Ondrej Kupka <ondra.cap@gmail.com>
2014-05-20 19:56:36 +0200

    Include meekod/daemon fixes

--- master/command.go
+++ master/command.go
@@ -7,6 +7,7 @@ package master
 
 import (
 	// Stdlib
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -22,10 +23,8 @@ import (
 	"github.com/tchap/gocli"
 )
 
-var configPath string
-
 var Command = &gocli.Command{
-	UsageLine: "master [-config=PATH]",
+	UsageLine: "master [-debug] [-config=PATH]",
 	Short:     "start a Cider build master node",
 	Long: `
   Start a build master node using the meekod-compatible configuration file
@@ -37,20 +36,30 @@ ENVIRONMENT:
 	Action: run,
 }
 
+var (
+	flagDebug  bool
+	flagConfig string
+)
+
 func init() {
-	Command.Flags.StringVar(&configPath, "config", configPath, "meekod-compatible config file")
+	Command.Flags.BoolVar(&flagDebug, "debug", flagDebug, "enable debug output")
+	Command.Flags.StringVar(&flagConfig, "config", flagConfig, "meekod-compatible config file")
 }
 
 func run(cmd *gocli.Command, args []string) {
 	if err := runWrapper(cmd, args); err != nil {
-		log.Fatalln("Error: ", err)
+		log.Fatalln("\nError:", err)
 	}
 }
 
 func runWrapper(cmd *gocli.Command, args []string) error {
 	// Set up logging.
 	log.SetFlags(0)
-	broklog.UseLogger(seelog.Default)
+	if flagDebug {
+		broklog.UseLogger(seelog.Default)
+	} else {
+		seelog.ReplaceLogger(seelog.Disabled)
+	}
 
 	// Make sure there were no arguments specified.
 	if len(args) != 0 {
@@ -59,22 +68,25 @@ func runWrapper(cmd *gocli.Command, args []string) error {
 	}
 
 	// Process input arguments and parameters.
-	if configPath == "" {
-		configPath = os.Getenv("CIDER_MASTER_CONFIG")
-		if configPath == "" {
+	if flagConfig == "" {
+		flagConfig = os.Getenv("CIDER_MASTER_CONFIG")
+		if flagConfig == "" {
 			log.Fatalln("Error: configuration file not specified")
 		}
 	}
 
 	// Create a daemon instance from the specified config file.
-	dmn, err := daemon.NewFromConfigAsFile(configPath, &daemon.Options{
+	dmn, err := daemon.NewFromConfigAsFile(flagConfig, &daemon.Options{
 		DisableSupervisor:     true,
 		DisableLocalEndpoints: true,
 	})
 	if err != nil {
 		return err
 	}
-	defer dmn.Terminate()
+	defer func() {
+		dmn.Terminate()
+		log.Println("The background thread terminated")
+	}()
 
 	// Start catching signals.
 	signalCh := make(chan os.Signal, 1)
@@ -84,21 +96,30 @@ func runWrapper(cmd *gocli.Command, args []string) error {
 	monitorCh := make(chan *broker.EndpointCrashReport)
 	dmn.Monitor(monitorCh)
 
+	// Start the daemon.
+	log.Println("Starting the background thread")
+	dmnErrorCh := make(chan error, 1)
+	go func() {
+		dmnErrorCh <- dmn.Serve()
+	}()
+
 	// Loop until interrupted.
 	for {
 		select {
 		case report, ok := <-monitorCh:
-			if report != nil && report.Dropped {
-				log.Printf("Endpoint %v dropped", report.FactoryId)
-				return report.Error
-			}
 			if !ok {
-				return nil
+				continue
 			}
-			log.Printf("Endpoint %v crashed with error=%v\n", report.FactoryId, report.Error)
+			if report.Dropped {
+				return fmt.Errorf("Endpoint %v dropped", report.FactoryId)
+			}
+			log.Printf("Endpoint %v crashed: %v\n", report.FactoryId, report.Error)
 
-		case sig := <-signalCh:
-			log.Printf("Signal received (%v), terminating...\n", sig)
+		case err := <-dmnErrorCh:
+			return err
+
+		case <-signalCh:
+			log.Println("Signal received, terminating...")
 			return nil
 		}
 	}
