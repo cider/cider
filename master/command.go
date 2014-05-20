40d58e3ed12937ae44e52714b23a4e3764ee89bd
Ondrej Kupka <ondra.cap@gmail.com>
2014-05-20 16:36:44 +0200

    master: Rewrite to use meeko/meekod/daemon package

--- master/command.go
+++ master/command.go
@@ -11,55 +11,46 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
-	// Cider
-	"github.com/cider/cider/utils"
-
-	// Cider
-	clog "github.com/meeko/meekod/broker/log"
+	// Meeko
+	"github.com/meeko/meekod/broker"
+	broklog "github.com/meeko/meekod/broker/log"
+	"github.com/meeko/meekod/daemon"
 
 	// Others
 	"github.com/cihub/seelog"
 	"github.com/tchap/gocli"
 )
 
-var (
-	listen    string
-	token     string
-	heartbeat time.Duration
-)
+var configPath string
 
 var Command = &gocli.Command{
-	UsageLine: `master [-listen=ADDRESS] [-token=TOKEN]
-	                   [-heartbeat=HEARTBEAT]`,
-	Short: "start a build master node",
+	UsageLine: "master [-config=PATH]",
+	Short:     "start a Cider build master node",
 	Long: `
-  Start a build master node and start listening on ADDRESS for build slave
-  connections. Every build slave must pass TOKEN in the connection request,
-  otherwise the connection is refused.
-
-  If HEARTBEAT is specified, the build master sends a ping message to every
-  build slave every HEARTBEAT and closes the connection if the slave is not
-  responding.
+  Start a build master node using the meekod-compatible configuration file
+  that can be found at PATH.
 
 ENVIRONMENT:
-  CIDER_MASTER_LISTEN - can be used instead of -listen
-  CIDER_MASTER_TOKEN  - can be used instead of -token
+  CIDER_MASTER_CONFIG - can be used instead of -config
 	`,
 	Action: run,
 }
 
 func init() {
-	Command.Flags.StringVar(&listen, "listen", listen, "network address to listen on")
-	Command.Flags.StringVar(&token, "token", token, "build master access token")
-	Command.Flags.DurationVar(&heartbeat, "heartbeat", heartbeat, "heartbeat period")
+	Command.Flags.StringVar(&configPath, "config", configPath, "meekod-compatible config file")
 }
 
 func run(cmd *gocli.Command, args []string) {
+	if err := runWrapper(cmd, args); err != nil {
+		log.Fatalln("Error: ", err)
+	}
+}
+
+func runWrapper(cmd *gocli.Command, args []string) error {
 	// Set up logging.
 	log.SetFlags(0)
-	clog.UseLogger(seelog.Default)
+	broklog.UseLogger(seelog.Default)
 
 	// Make sure there were no arguments specified.
 	if len(args) != 0 {
@@ -67,30 +58,50 @@ func run(cmd *gocli.Command, args []string) {
 		os.Exit(2)
 	}
 
-	// Read the environment to fill in the missing parameters.
-	utils.GetenvOrFailNow(&listen, "CIDER_MASTER_LISTEN", cmd)
-	utils.GetenvOrFailNow(&token, "CIDER_MASTER_TOKEN", cmd)
+	// Process input arguments and parameters.
+	if configPath == "" {
+		configPath = os.Getenv("CIDER_MASTER_CONFIG")
+		if configPath == "" {
+			log.Fatalln("Error: configuration file not specified")
+		}
+	}
+
+	// Create a daemon instance from the specified config file.
+	dmn, err := daemon.NewFromConfigAsFile(configPath, &daemon.Options{
+		DisableSupervisor:     true,
+		DisableLocalEndpoints: true,
+	})
+	if err != nil {
+		return err
+	}
+	defer dmn.Terminate()
 
 	// Start catching signals.
 	signalCh := make(chan os.Signal, 1)
 	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM)
 
-	// Start listening for slave connections.
-	m := New(listen, token).EnableHeartbeat(heartbeat).Listen()
-	log.Printf("Cider broker listening on %v\n", listen)
+	// Register a monitoring channel.
+	monitorCh := make(chan *broker.EndpointCrashReport)
+	dmn.Monitor(monitorCh)
 
-	select {
-	// Wait for a signal, then terminate the master in a clean way.
-	case <-signalCh:
-		log.Println("Interrupted, exiting...")
-		m.Terminate()
+	// Loop until interrupted.
+	for {
+		select {
+		case report, ok := <-monitorCh:
+			if report != nil && report.Dropped {
+				log.Printf("Endpoint %v dropped", report.FactoryId)
+				return report.Error
+			}
+			if !ok {
+				return nil
+			}
+			log.Printf("Endpoint %v crashed with error=%v\n", report.FactoryId, report.Error)
 
-	// Unblock also in case the master node has crashed.
-	case <-m.Terminated():
+		case sig := <-signalCh:
+			log.Printf("Signal received (%v), terminating...\n", sig)
+			return nil
+		}
 	}
 
-	// Check the master exit status and exit the process accordingly.
-	if err := m.Wait(); err != nil {
-		log.Fatal(err)
-	}
+	return nil
 }
