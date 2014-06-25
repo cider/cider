a7b33286674b160da752cfae4ad7d0088e6ff045
Ondrej Kupka <ondra.cap@gmail.com>
2014-06-25 11:50:38 +0200

    Disable the master subcommand

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"os"
 
 	"github.com/cider/cider/build"
-	"github.com/cider/cider/master"
+//	"github.com/cider/cider/master"
 	"github.com/cider/cider/slave"
 
 	"github.com/tchap/gocli"
@@ -39,7 +39,7 @@ func main() {
   To understand more about how Cider works, check the available subcommands.`
 
 	cider.MustRegisterSubcommand(build.Command)
-	cider.MustRegisterSubcommand(master.Command)
+//	cider.MustRegisterSubcommand(master.Command)
 	cider.MustRegisterSubcommand(slave.Command)
 
 	cider.Run(os.Args[1:])
